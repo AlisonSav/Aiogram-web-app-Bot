68894cbc34c5348f674c830d4b880e2910b0b5ca
Alison Sav <alisa.zarytska@troops-ext.fr>
2023-10-04 15:04:39 +0200

    aiogram web_app bot

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
+import config
 
-bot = Bot('5795089416:AAGvY6p8jdPg4EDoHxbUEMcSR1xIWpXofDg')
+bot = Bot(config.BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
@@ -17,5 +18,16 @@ async def start(message: types.Message):
 async def web_app(message: types.Message):
     res = json.loads(message.web_app_data.data)
     await message.answer(f"Name: {res['name']}. Email: {res['email']}. Phone: {res['phone']}")
+
+
+@dp.message_handler(commands=["payment"])
+async def payment(message: types.Message):
+    await bot.send_invoice(message.chat.id, "Buy product", "Product", "invoice", config.PAYMENT_TOKEN, "USD",
+                           [types.LabeledPrice("Buy product", 5 * 100)])
+
+
+@dp.message_handler(content_types=types.ContentType.SUCCESSFUL_PAYMENT)
+async def success(message: types.Message):
+    await message.answer(f"Success: {message.successful_payment.order_info}")
 
 executor.start_polling(dp)
